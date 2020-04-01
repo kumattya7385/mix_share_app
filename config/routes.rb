bc186264c4463d24d7d10c3d1679cb10996df24e
kumattya7385 <kuma.tya7385@gmail.com>
2020-04-02 00:29:20 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get   'top_pages/home'
-  get   'top_pages/contact'
   root  'top_pages#home'
+  get   '/contact', to: 'top_pages#contact'
 end
--- test/controllers/top_pages_controller_test.rb
+++ test/controllers/top_pages_controller_test.rb
@@ -3,17 +3,12 @@ require 'test_helper'
 class TopPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
-  end
-  test "should get home" do
-    get top_pages_home_url
-    assert_response :success
-    assert_select "title", "Vmixsh"
   end
 
   test "should get contact" do
-    get top_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "問い合わせ - Vmixsh"
   end
