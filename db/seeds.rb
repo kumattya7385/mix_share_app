519aed0a57daf6bd4465a4d3a55b4e8cb856c203
kumattya7385 <kuma.tya7385@gmail.com>
2020-05-12 20:20:23 +0900

    heroku db:seed setting change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,24 @@
-if Rails.env == 'development'
-  User.create!(name:  "Example User",
-    email: "example@vmish.com",
-    password:              "foobar",
-    password_confirmation: "foobar",
-    admin: true,
-    activated: true,
-    content: "こんにちは。これはユーザープロフィール用のテストです。表示が上手くいっているといいです。"
-  )
+User.create!(name:  "Example User",
+  email: "example@vmish.com",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  admin: true,
+  activated: true,
+  content: "こんにちは。これはユーザープロフィール用のテストです。表示が上手くいっているといいです。"
+)
 
-  User.create!(name:  "ExampleKuma",
-    email: "TestUser@vmish.com",
-    password:              "foobar",
-    password_confirmation: "foobar",
-    activated: true
-  )
+User.create!(name:  "TestUser",
+  email: "TestUser@vmish.com",
+  password:              "foobar",
+  password_confirmation: "foobar",
+  activated: true
+)
 
-  (1..20).each do |i|
-    Item.create(
-      content: "テストテキスト#{i}",
-      impressions_count: rand(100),
-      title: "テストタイトル#{i}",
-      user_id: rand(2)+1
-    )
-  end
+(1..20).each do |i|
+  Item.create(
+    content: "テストテキスト#{i}",
+    impressions_count: rand(100),
+    title: "テストタイトル#{i}",
+    user_id: rand(2)+1
+  )
 end
